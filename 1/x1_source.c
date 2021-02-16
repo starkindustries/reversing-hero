8a452534e09a5c1547775c23cf9844fcde9008ca
Zion <zionperez@gmail.com>
2021-02-16 11:56:34 -0700

    Solved part1. Update x1_source with read_file function.

--- 1/x1_source.c
+++ 1/x1_source.c
@@ -1,57 +1,67 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-// char *read_file(char *buffer, long max_chars, FILE *file_pointer)
-// {
-//     char char_in_buffer_at_strcspn_result;
-//     char *return_buffer;
-//     size_t strcspn_result;
-//     undefined8 default_return_var;
+char *read_file(char *buffer, long max_chars, FILE *file_pointer)
+{
+    char char_in_buffer_at_strcspn_result;
+    char *return_buffer;
+    size_t strcspn_result;
+    char *default_return_var;
 
-//     if (max_chars != 0)
-//     {
-//         return_buffer = fgets(buffer, (int)max_chars, file_pointer);
-//         if (return_buffer != (char *)0x0)
-//         {
-//             strcspn_result = strcspn(buffer, &global_0x0a00);
-//             char_in_buffer_at_strcspn_result = buffer[strcspn_result];
-//             buffer[strcspn_result] = '\0';
-//             if (char_in_buffer_at_strcspn_result == '\n')
-//             {
-//                 return buffer;
-//             }
-//         }
-//         default_return_var = 0;
-//     }
-//     return (char *)default_return_var;
-// }
+    char newline_null[] = {0x0a, 0x00};
+
+    if (max_chars != 0)
+    {
+        return_buffer = fgets(buffer, (int)max_chars, file_pointer);
+        if (return_buffer != (char *)0x0)
+        {
+            strcspn_result = strcspn(buffer, &newline_null);
+            char_in_buffer_at_strcspn_result = buffer[strcspn_result];
+            buffer[strcspn_result] = '\0';
+            if (char_in_buffer_at_strcspn_result == '\n')
+            {
+                return buffer;
+            }
+        }
+        default_return_var = 0;
+    }
+    return (char *)default_return_var;
+}
 
 int main()
 {
     int __status;
     char *return_buffer;
-    // undefined4 extraout_var;
 
-    // globals
-    char question_mark[] = "?\n";
+    char question_mark[] = "? ";
+    char exclamation_plus[] = "! + ";
+    char exclamation_minus[] = "! - \n";
     char r_string[] = "r";
+    char buffer[0x1b];
+    
+    // This is the string that the program compares the user input to
+    // Same string w/o double backslash: 
+    // d(-_-)b//d(+_+)b\\d(-_-)b
+    // Above is easier to copy/paste
+    char emoji_string[] = "d(-_-)b//d(+_+)b\\\\d(-_-)b";    
 
     printf(&question_mark);
     FILE *file_pointer = fdopen(0, &r_string);
-    //     return_buffer = (char *)read_file(&global_buffer, 0x1b, file_pointer);
-    //     if (return_buffer != (char *)0x0)
-    //     {
-    //     __status = strcmp(&global_buffer,s_d(-_-)b//d(+_+)b\\d(-_-)b_00601065);
-    //     if (CONCAT44(extraout_var,__status) == 0) {
-    //             printf(&exclamation_plus);
-    //             target_func(&global_buffer);
-    //             __status = 0;
-    //             goto LAB_004005ab;
-    //     }
-    //     }
-    //     printf(&exclamation_minus);
-    //     __status = 1;
-    // LAB_004005ab:
-    //     fclose(file_pointer);
-    //     /* WARNING: Subroutine does not return */
-    //     exit(__status);
+    return_buffer = (char *)read_file(&buffer, 0x1b, file_pointer);
+    if (return_buffer != (char *)0x0)
+    {
+        __status = strcmp(&buffer, emoji_string);
+        if (__status == 0)
+        {
+            printf(&exclamation_plus);
+            printf("** Target Function **\n");
+            // target_func(&global_buffer);
+            fclose(file_pointer);
+            return 0;
+        }
+    }
+    printf(&exclamation_minus);
+    fclose(file_pointer);
+    return 0;
 }
